cd771405955ca3b08b71f924e3fd1acc73013ff9
Lukina Olga <lukina0l@yandex.ru>
2019-03-27 02:06:19 +0100

    add api func

--- src/game-service.js
+++ src/game-service.js
@@ -4,6 +4,11 @@
  */
 const apiUrl = 'http://localhost:3000';
 
+const JSON_HEADERS = {
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'
+};
+
 export async function getGames() {
   const resp = await fetch(`${apiUrl}/game`);
   return await resp.json();
@@ -11,5 +16,23 @@ export async function getGames() {
 
 export async function deleteGame(gameId) {
   const resp = await fetch(`${apiUrl}/game/${gameId}`, { method: 'DELETE' });
+  return await resp.json();
+}
+
+export async function addGame(name) {
+  const resp = await fetch(`${apiUrl}/game`, {
+    method: 'POST',
+    headers: JSON_HEADERS,
+    body: JSON.stringify({name})
+  });
+  return await resp.json();
+}
+
+export async function editGame(gameId, name) {
+  const resp = await fetch(`${apiUrl}/game/${gameId}`, {
+    method: 'PUT',
+    headers: JSON_HEADERS,
+    body: JSON.stringify({name})
+   });
   return await resp.json();
 }
