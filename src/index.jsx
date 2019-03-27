85042f0a6b549319314a895b14d91477895bb157
Lukina Olga <lukina0l@yandex.ru>
2019-03-27 02:06:36 +0100

    add some logig

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-import { getGames, deleteGame } from './game-service';
+import { getGames, deleteGame, addGame, editGame } from './game-service';
 
 class App extends React.Component {
   state = {
     games: [],
+    newGameName: ''
   };
 
   componentDidMount = async () => {
@@ -23,21 +24,56 @@ class App extends React.Component {
     this.getGames();
   };
 
+  addGame = async () => {
+    await addGame(this.state.newGameName);
+    this.getGames();
+  };
+
+  editGame = async (id, name) => {
+    await editGame(id, name);
+    this.getGames();
+  };
+
+  handleChange = event => {
+    this.setState({
+      newGameName: event.target.value
+    });
+  };
+
+  handleChangeName = (event, index) => {
+    let games = [...this.state.games];
+    games[index].name = event.target.value;
+    this.setState({ games });
+  };
+
   render() {
     const { games } = this.state;
+
     return (
       <div>
-        <h1>Game Library</h1>
+        <h2>Add game</h2>
+        <hr />
+        <input
+          placeholder="New game"
+          name="name"
+          value={this.state.newGameName}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.addGame}><i class="fas fa-plus"></i></button>
+        <h2>Game Library</h2>
         <hr />
         {games &&
-          games.map(game => (
+          games.map((game, index) => (
             <div key={game.id}>
-              <label>{game.name}</label>
-              <button>edit</button>
-              <button onClick={() => this.deleteGame(game.id)}>delete</button>
+              <input
+                name="name"
+                value={game.name}
+                onChange={(event) => this.handleChangeName(event, index)}
+                />
+              <button onClick={() => this.editGame(game.id, game.name)}><i class="fas fa-check-circle"></i></button>
+              <button onClick={() => this.deleteGame(game.id)}><i class="fas fa-trash-alt"></i></button>
             </div>
           ))}
-          <input placeholder="game name" /><button>add new game</button>
       </div>
     );
   }
